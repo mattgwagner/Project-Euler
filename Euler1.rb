b19b82816bfe5becbaf0318c89ec5b805630b319
Matt <>
2010-02-13 14:34:45 -0500

    made euler 1 cool looking

--- Euler1.rb
+++ Euler1.rb
@@ -2,12 +2,4 @@
 #multiples of 3 or 5, we get 3, 5, 6 and 9. 
 #The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
-sum = 0
-
-for x in (1...1000)
-	if x % 3 == 0 or x % 5 == 0
-		sum += x
-	end
-end
-
-puts sum
+puts (1...1000).select{|x| x % 3 == 0 or x % 5 == 0}.inject{|sum, curr| sum += curr}
