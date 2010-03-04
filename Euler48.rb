d55cc143bd963e6c8197f0c95a6fececa9fee043
mattgwagner <mattgwagner@gmail.com>
2010-03-03 23:32:02 -0500

    update to Euler 48

--- Euler48.rb
+++ Euler48.rb
@@ -1,10 +1,4 @@
 #The series, 1^(1) + 2^(2) + 3^(3) + ... + 10^(10) = 10405071317.
 
 #Find the last ten digits of the series, 1^(1) + 2^(2) + 3^(3) + ... + 1000^(1000).
-sum=0
-
-for i in (1...1000)
-	sum+= (i**i)
-end
-
-print sum.to_s()[-10..-1]
+puts (1...1000).inject{|sum,x| sum + (x**x)}.to_s()[-10..-1]
