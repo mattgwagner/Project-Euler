b02df45a35b0dc8557fd65679aad32aa41d3ad91
mattgwagner <mattgwagner@gmail.com>
2010-03-15 00:33:08 -0400

    clean up of the one c-sharp solution in here

--- Euler19.cs
+++ Euler19.cs
@@ -4,59 +4,21 @@ using System.Text;
 
 public class MyClass
 {
-	public static void RunSnippet()
+	public static void Main()
 	{
 		int count = 0;
+		
 		DateTime d = new DateTime(1901, 1, 1);
 		DateTime end = new DateTime(2000,12,31);
 		
 		while(d < end)
 		{
-            Console.WriteLine(d.ToShortDateString());
-
 			if(d.DayOfWeek == DayOfWeek.Sunday)
 				count++;
 			
 			d = d.AddMonths(1);
 		}
 		
-		WL(count);
+		Console.WriteLine(count);
 	}
-	
-	#region Helper methods
-	
-	public static void Main()
-	{
-		try
-		{
-			RunSnippet();
-		}
-		catch (Exception e)
-		{
-			string error = string.Format("---\nThe following error occurred while executing the snippet:\n{0}\n---", e.ToString());
-			Console.WriteLine(error);
-		}
-		finally
-		{
-			Console.Write("Press any key to continue...");
-			Console.ReadKey();
-		}
-	}
-
-	private static void WL(object text, params object[] args)
-	{
-		Console.WriteLine(text.ToString(), args);	
-	}
-	
-	private static void RL()
-	{
-		Console.ReadLine();	
-	}
-	
-	private static void Break() 
-	{
-		System.Diagnostics.Debugger.Break();
-	}
-
-	#endregion
 }
