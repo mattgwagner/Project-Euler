ae929ec8ceecbf3001d1b1c6c4560e611fc50cd6
mattgwagner <mattgwagner@gmail.com>
2010-05-02 15:59:18 -0400

    finished 13 for pyZ

--- Euler13.py
+++ Euler13.py
@@ -1,7 +1,5 @@
 #Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 file = open('Euler13.txt')
-sum = 0
 
-for l in file.readlines():
-    sum += int(l)
-print(str(sum)[:10])
+print str(sum(int(l) for l in file.readlines()))[:10]
+
