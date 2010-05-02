a7c430370371c2f863d26aa27c18e3f7cd4b4bba
mattgwagner <mattgwagner@gmail.com>
2010-05-02 15:58:48 -0400

    finished euler 10 for python

--- Euler10.py
+++ Euler10.py
@@ -3,9 +3,4 @@
 ##Find the sum of all the primes below two million.
 from prime import isPrime
 
-max = 2000000
-sum = 1
-for x in range(1,max,2):
-    if isPrime(x):
-        sum += x
-print(sum)
+print sum(x for x in range(1,2000000,2) if isPrime(x))
