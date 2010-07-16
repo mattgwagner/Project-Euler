e7d9bc5de5bbc673af9f90f0c5a21a67e2b234cc
Matt Wagner <mattgwagner@gmail.com>
2010-07-16 15:09:28 -0400

    making euler 36 incredibly unreadable, just because i can

--- Euler36.rb
+++ Euler36.rb
@@ -1,11 +1,3 @@
 #Find the sum of all numbers, less than one million,
 #which are palindromic in base 10 and base 2.
-sum = 0
-
-(1...1000000).each do|x|
-	if x == x.to_s.reverse.to_i and x.to_s(2).to_i == x.to_s(2).reverse.to_i
-		sum += x
-	end
-end
-
-puts sum
+print (1...1000000).select{|x| x == x.to_s.reverse.to_i and x.to_s(2).to_i == x.to_s(2).reverse.to_i}.inject{|sum,x| sum += x}
