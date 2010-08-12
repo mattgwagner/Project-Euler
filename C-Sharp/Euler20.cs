cbde46695f83356cce453e476e7a775c37c969ea
mattgwagner <none@none>
2010-08-12 14:21:44 -0400

    finished 20 in c-sharp, added factorial method to base class for later use

---
+++ C-Sharp/Euler20.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using System.Numerics;
+
+class Euler20 : ProjectEuler.EulerProblem
+{
+    public override BigInteger solution()
+    {
+        return factorial(100).ToString().Sum(c => int.Parse(c.ToString()));
+    }
+}
--- C-Sharp/Main.cs
+++ C-Sharp/Main.cs
@@ -27,5 +27,15 @@ namespace ProjectEuler
     public abstract class EulerProblem
     {
         public abstract BigInteger solution();
+
+        protected BigInteger factorial(BigInteger f)
+        {
+            return f != 1 ? (f * factorial(f - 1)) : 1;
+        }
+
+        protected Boolean isPrime(BigInteger p)
+        {
+            return false;
+        }
     }
 }
