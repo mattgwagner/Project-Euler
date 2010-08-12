9e3f737abc8510121e5da2b79579aa56ee30fd22
mattgwagner <none@none>
2010-08-12 14:09:16 -0400

    finished 16 in c#

---
+++ C-Sharp/Euler16.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using System.Numerics;
+
+class Euler16 : ProjectEuler.EulerProblem
+{
+    public override BigInteger solution()
+    {
+        return BigInteger.Pow(2, 1000).ToString().Sum(c => int.Parse(c.ToString()));
+    }
+}
