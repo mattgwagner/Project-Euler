5fe1c82cb527a1f639baeffe42af19cbaa48e770
mattgwagner <none@none>
2010-08-12 13:53:31 -0400

    euler2 in c-sharp

---
+++ C-Sharp/Euler2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+class Euler2 : ProjectEuler.EulerProblem
+{
+    public override BigInteger solution()
+    {
+        int current = 2;
+        int previous = 1;
+        int tmp = -1;
+
+        int sum = 2; //starts @ 2 to accomidate current
+
+        while (current < 4000000)
+        {
+            tmp = current;
+            current = previous + current;
+            previous = tmp;
+
+            if (current % 2 == 0)
+            {
+                sum += current;
+            }
+        }
+
+        return sum;
+    }
+}
