e8fbea16e156640dd42f9236547515a8a7d831bd
mattgwagner <none@none>
2010-08-12 14:14:54 -0400

    completed 6 in c#, woot

---
+++ C-Sharp/Euler6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+class Euler6 : ProjectEuler.EulerProblem
+{
+    public override BigInteger solution()
+    {
+        BigInteger sqOfSums = BigInteger.Pow(Enumerable.Range(1, 100).Sum(), 2);
+        BigInteger sumOfSqs = Enumerable.Range(1, 100).Sum(i => i * i);
+
+        return sqOfSums - sumOfSqs;
+    }
+}
