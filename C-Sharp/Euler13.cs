f62fa41dc8481729b28405d15110d54a62b87d28
mattgwagner <none@none>
2010-12-16 16:01:53 -0500

    finished 13 in c#

---
+++ C-Sharp/Euler13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler;
+using System.IO;
+using System.Numerics;
+
+class Euler13 : EulerProblem
+{
+    public override object Solution()
+    {
+        return GetNumbers().Sum().ToString().Substring(0, 10);
+    }
+
+    protected IEnumerable<BigInteger> GetNumbers()
+    {
+        string str;
+
+        using (StreamReader rdr = new StreamReader("Euler13.txt"))
+        {
+            while ((str = rdr.ReadLine()) != null)
+            {
+                yield return BigInteger.Parse(str);
+            }
+        }
+    }
+}
