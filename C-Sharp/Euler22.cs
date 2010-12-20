8ea225c36d0173dd88fe3c1958723f23679ae548
mattgwagner <none@none>
2010-12-19 21:44:04 -0500

    Did 22 in C#

---
+++ C-Sharp/Euler22.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ProjectEuler;
+
+class Euler22 : EulerProblem
+{
+    const String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public override object Solution()
+    {
+        var names = GetNames();
+
+        names.Sort();
+
+        int num = 1;
+        int total = 0;
+
+        foreach (var name in names)
+        {
+            total += num * name.Sum(c => alphabet.IndexOf(c) + 1);
+
+            num++;
+        }
+
+        return total;
+    }
+
+    List<String> GetNames()
+    {
+        using (StreamReader rdr = new StreamReader("Euler22.txt"))
+        {
+            return new List<String>(rdr.ReadToEnd().Replace("\"", "").Split(','));
+        }
+    }
+}
