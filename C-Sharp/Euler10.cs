d231c7c55bcb7ee2da4af6c861cafe6d84ddc009
mattgwagner <none@none>
2010-12-29 10:16:43 -0500

    Completed euler 10

---
+++ C-Sharp/Euler10.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using ProjectEuler;
+
+class Euler10 : EulerProblem
+{
+    public override object Solution()
+    {
+        BigInteger bi = new BigInteger();
+
+        foreach (var i in FindPrimesUpToMax(2000000))
+        {
+            bi += i;
+        }
+
+        return bi;
+    }
+}
