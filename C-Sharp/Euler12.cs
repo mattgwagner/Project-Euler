9141fc2cf2e06a0062fce430523004b616a27c5c
mattgwagner <none@none>
2011-01-05 21:25:59 -0500

    Moving some files around
    --HG--
    rename : Euler1.py => Python/Euler1.py
    rename : Euler10.py => Python/Euler10.py
    rename : Euler13.py => Python/Euler13.py
    rename : Euler14.py => Python/Euler14.py
    rename : Euler2.py => Python/Euler2.py
    rename : Euler20.py => Python/Euler20.py
    rename : Euler21.py => Python/Euler21.py
    rename : Euler22.py => Python/Euler22.py
    rename : Euler29.py => Python/Euler29.py
    rename : Euler3.py => Python/Euler3.py
    rename : Euler35.py => Python/Euler35.py
    rename : Euler4.py => Python/Euler4.py
    rename : Euler48.py => Python/Euler48.py
    rename : Euler5.py => Python/Euler5.py
    rename : Euler53.py => Python/Euler53.py
    rename : Euler6.py => Python/Euler6.py
    rename : Euler7.py => Python/Euler7.py
    rename : Euler8.py => Python/Euler8.py
    rename : Euler9.py => Python/Euler9.py
    rename : prime.py => Python/prime.py
    rename : Euler1.rb => Ruby/Euler1.rb
    rename : Euler10.rb => Ruby/Euler10.rb
    rename : Euler12.rb => Ruby/Euler12.rb
    rename : Euler13.rb => Ruby/Euler13.rb
    rename : Euler14.rb => Ruby/Euler14.rb
    rename : Euler16.rb => Ruby/Euler16.rb
    rename : Euler2.rb => Ruby/Euler2.rb
    rename : Euler20.rb => Ruby/Euler20.rb
    rename : Euler21.rb => Ruby/Euler21.rb
    rename : Euler22.rb => Ruby/Euler22.rb
    rename : Euler29.rb => Ruby/Euler29.rb
    rename : Euler3.rb => Ruby/Euler3.rb
    rename : Euler35.rb => Ruby/Euler35.rb
    rename : Euler36.rb => Ruby/Euler36.rb
    rename : Euler4.rb => Ruby/Euler4.rb
    rename : Euler48.rb => Ruby/Euler48.rb
    rename : Euler5.rb => Ruby/Euler5.rb
    rename : Euler6.rb => Ruby/Euler6.rb
    rename : Euler7.rb => Ruby/Euler7.rb
    rename : Euler8.rb => Ruby/Euler8.rb
    rename : Euler9.rb => Ruby/Euler9.rb
    rename : Util.rb => Ruby/Util.rb

---
+++ C-Sharp/Euler12.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ProjectEuler;
+using System.Numerics;
+using System;
+
+class Euler12 : EulerProblem
+{
+    public override object Solution()
+    {
+        foreach (BigInteger i in GetTriangleNumbers())
+        {
+            if (GetDivisors(i).Count() > 500) return i;
+        }
+
+        return -1;
+    }
+
+    IEnumerable<BigInteger> GetDivisors(BigInteger x)
+    {
+        for (int i = 1; i < (x / 2) + 1; i++)
+        {
+            if (x % i == 0) yield return i;
+        }
+
+        yield return x;
+    }
+
+    IEnumerable<BigInteger> GetTriangleNumbers()
+    {
+        int current = 10000000;
+        BigInteger sum = 0;
+
+        while (true)
+        {
+            yield return sum += current++;
+        }
+    }
+}
---
+++ C-Sharp/Euler24.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProjectEuler;
+
+class Euler24 : EulerProblem
+{
+    const string digits = "0123456789";
+
+    public override object Solution()
+    {
+        throw new NotImplementedException();
+    }
+}
