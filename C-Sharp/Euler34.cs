57a6113bd98855a3e31e238266c9b3b18fa4a55a
mwagner-local <none@none>
2011-07-20 11:46:20 -0400

    Finished 34 in C#

---
+++ C-Sharp/Euler34.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using ProjectEuler;
+
+class Euler34 : EulerProblem
+{
+    public override object Solution()
+    {
+        return Enumerable.Range(3, 100000).Where(s => Check(s)).Sum();
+    }
+
+    Boolean Check(BigInteger bi)
+    {
+        BigInteger sum = 0;
+
+        foreach (var c in bi.ToString())
+        {
+            sum += BigInteger.Parse(c + "").Factorial();
+        }
+
+        return bi == sum;
+    }
+}
