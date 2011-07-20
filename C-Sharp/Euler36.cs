8aef30e927fc1c128971b057c1c4c0c47377072c
mwagner-local <none@none>
2011-07-20 12:01:01 -0400

    Finished 36 in C#

---
+++ C-Sharp/Euler36.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using ProjectEuler;
+
+/* Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2. */
+public class Euler36 : EulerProblem
+{
+    public override object Solution()
+    {
+        var range = from r in Enumerable.Range(1, 1000000)
+                    where r.ToString().isPalindrome() &&
+                    Convert.ToString(r, 2).isPalindrome()
+                    select r;
+
+        return range.Sum();
+    }
+}
