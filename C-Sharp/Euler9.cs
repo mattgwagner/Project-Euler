3b245bc351524a98b08386cacf7f421e1aac5d0d
mwagner-local <none@none>
2011-07-20 10:46:57 -0400

    Work on Euler 9, removed Haskell false start

---
+++ C-Sharp/Euler9.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Euler9 : ProjectEuler.EulerProblem
+{
+    public override object Solution()
+    {
+        for (int a = 1; a < 1000; a++)
+        {
+            for (int b = (a + 1); b < 1000; b++)
+            {
+                Double c = Math.Sqrt((a * a) + (b * b));
+
+                if (a < b && b < c && (a + b + c) == 1000)
+                {
+                    return (a * b * c);
+                }
+            }
+        }
+
+        throw new NotFiniteNumberException("Uh oh");
+    }
+}
