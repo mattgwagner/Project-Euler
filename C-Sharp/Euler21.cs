13a0382f5e9f91956f5d534c20830e69c3cd8b1f
mwagner-local <none@none>
2011-07-20 11:04:38 -0400

    Finished euler21 in c#

---
+++ C-Sharp/Euler21.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Euler21 : ProjectEuler.EulerProblem
+{
+    public override object Solution()
+    {
+        int sum = 0;
+
+        foreach (int i in Enumerable.Range(1, 10000))
+        {
+            int res = GetDivisors(i).Sum();
+
+            if (i != res && i == GetDivisors(res).Sum())
+            {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    IEnumerable<int> GetDivisors(int num)
+    {
+        return Enumerable.Range(1, num).Where(i => num % i == 0 && i != num);
+    }
+}
